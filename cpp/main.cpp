cf563ca433731e7c5ad7e8b1842bded1a8dfa4f0
estnml <lightofpast@gmail.com>
2025-06-24 23:58:28 +0300

    dayan

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
-
-class ListNode
-{
-
-public:
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
 
 class Solution
 {
 public:
-  ListNode *reverseList(ListNode *head)
-  {
-    std::cout << "hello";
-    return nullptr;
-  }
+	bool hasDuplicate(std::vector<int> &nums)
+	{
+		std::map<int, int> num_occurence_map;
+		std::for_each(nums.begin(), nums.end(), [&](int n) -> void
+					  { num_occurence_map[n]++; });
+
+		for (const auto &pair : num_occurence_map)
+		{
+			if (pair.second > 1)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 };
 
 int main(int argc, char **argv)
 {
-  auto solver = Solution();
-
-  ListNode *list1 = new ListNode(1);
-  list1->next = new ListNode(2);
-  list1->next->next = new ListNode(3);
-  list1->next->next->next = new ListNode(4);
-  list1->next->next->next->next = new ListNode(5);
-
-  auto reversed_list = solver.reverseList(list1);
-  std::cout << reversed_list;
+	return 0;
 }
